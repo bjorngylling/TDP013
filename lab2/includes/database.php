c5e2e552c61f06d69c0a1a948fdae89ed9f5c3fe
Björn Gylling <bjorn.gylling@gmail.com>
2010-09-03 14:37:32 +0200

    Function for mysql connection

--- lab2/includes/database.php
+++ lab2/includes/database.php
@@ -8,37 +8,38 @@ include 'database_settings.php';
 
 class Database extends Database_Settings {
   
-  public function connect() {
+  public function __construct() {
+    $this->connect();
+  }
+  
+  private function connect() {
     if($this->con) {
       return true; // We're already connected
     }
     
-    // Connect and select the database
-    // TODO: This part might need some error-checking, 
-    // making sure the connection succeeded before selecting table.
-    $connection = mysql_connect($this->db_host, 
-                                $this->db_user, 
-                                $this->db_pass);
-    $select_database = mysql_select_db($this->db_name, $connection);
+    // Connect to the database
+    // TODO: This part might need some error-checking
+    $this->con = new mysqli($this->db_host, 
+                             $this->db_user, 
+                             $this->db_pass,
+                             $this->db_name);
+                             
+    return true;
   }
   
   public function disconnect() {
-    
+    if($this->con) {
+      $this->con->close();
+      $this->con = false;
+    }
   }
   
-  public function select() {
-    
-  }
-  
-  public function insert() {
-    
-  }
-  
-  public function delete() {
-    
+  public function query($query, $parameters) {
+    foreach($parameters as $param) {
+      
+    }
   }
   
 }
 
 $db = new Database();
-$db->connect();
