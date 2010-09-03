9dd25b7f5cfb90daaa1bc967c4f5418cd062b314
Björn Gylling <bjorn.gylling@gmail.com>
2010-09-03 19:50:22 +0200

    Might be a good idea to include changed config file in commit...

--- lab2/includes/config.php
+++ lab2/includes/config.php
@@ -1,12 +1,24 @@
 <?php
-/* includes/database_settings.php
-   Database settings
-   
+/* includes/config.php
+   Application settings
+   Stored in a singleton class
 */
 
-class Database_Settings {
-  protected $db_host = '';
-  protected $db_user = '';
-  protected $db_pass = '';
-  protected $db_name = '';
+class Config {  
+  static private $instance = false;
+  
+  public $db_host = '';
+  public $db_user = '';
+  public $db_pass = '';
+  public $db_name = '';
+  public $db_salt = 'CHANGE THIS';
+  
+  static public function get_instance() {
+    if (!self::$instance) {
+      self::$instance = new Config;
+    }
+ 
+    return self::$instance;
+  }
+
 }
