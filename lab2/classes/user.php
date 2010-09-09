205de765ac48784fd90af717ebad0b2819081d9f
Björn Gylling <bjorn.gylling@gmail.com>
2010-09-09 17:00:21 +0200

    Begun writing sign_in function and load_user for User class.

--- lab2/classes/user.php
+++ lab2/classes/user.php
@@ -15,10 +15,33 @@ class User {
   
   function __construct($id) {
     $this->config = Config::get_instance();
+    $this->load_user($id);
   }
   
   public static function create_new($username, $password) {
     
+  }
+  
+  public static function sign_in($username, $password) {
+    $user = Database::get_instance()->query(
+      "SELECT username, stylesheet_id 
+       FROM users 
+       WHERE username = ? 
+       AND password = ?", 
+       $username,
+       $this->hash_password($password));
+    
+    return new User($user[0]['id']);
+  }
+  
+  private function load_user($id) {
+    $user = Database::get_instance()->query("SELECT username, stylesheet_id FROM users WHERE id = ?", $id);
+    $this->username = $user['username'];
+    $this->stylesheet_id = $user['stylesheet_id'];
+  }
+  
+  private function hash_password($password) {
+    return sha1($password.$this->config->db_salt);
   }
   
 }
